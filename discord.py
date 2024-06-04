e6c718690a374459ede9fb015f6b0dfa2f48e572
nekoy3 <84169441+nekoy3@users.noreply.github.com>
2024-06-04 15:17:24 +0900

    embedでデータを送信するメソッドの追加

--- discord.py
+++ discord.py
@@ -23,12 +23,54 @@ def sent_discord(s):
                 "title"         : "ギフト監視bot",
                 "description"   : s,
                 "timestamp"     : formatted_time,
-                "color"         : 5620992,
+                "color"         : 11027200,
             }
         ]
     }
   }
+  payload2['payload_json'] = json.dumps( payload2['payload_json'], ensure_ascii=False )
+  res = requests.post(webhook_url, data = payload2 )
+
+def sent_discord_embed(name, value, id):
+  url = "https://beterugift.jp/assets/img/icon/" + str(id) +".jpg"
+  # 現在の時刻を取得
+  current_time = datetime.now()
+
+  # 指定された形式で時刻を表示
+  formatted_time = current_time.strftime("%Y-%m-%dT%H:%M:%S%z")
+  try:
+    webhook_url  = get_webhook()
+  except Exception as e:
+    print(str(e))
+  #print(url)
+  payload2 = {
+    "payload_json" : {
+        "username"      :"ギフト監視bot",
+        "avatar_url"    : "https://beterugift.jp/assets/img/logo.png",
+        "embeds": [
+            {
+                "title"         : "ギフト監視bot",
+                "timestamp"     : formatted_time,
+                "color"         : 7419530,
+                "image"         : {
+                      "url" : url         
+                },
+                "footer": {
+                    "text" : "ベテルギフト",
+                },
+                "fields": [
+                    {
+                        "name"  : name,
+                        "value" : value,
+                        "inline": True,
+                    }
+                ]
+            }
+        ]
+      }
+    }
 
   ### embed付き
   payload2['payload_json'] = json.dumps( payload2['payload_json'], ensure_ascii=False )
   res = requests.post(webhook_url, data = payload2 )
+
