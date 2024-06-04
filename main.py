0dbd42221320c2d327327db3af7d6611a75916b3
nekoy3 <84169441+nekoy3@users.noreply.github.com>
2024-06-04 12:05:05 +0900

    ログレベルの追加、メッセージ送信メソッドの追加

--- main.py
+++ main.py
@@ -3,13 +3,26 @@
 
 from file_io import read_list_from_file
 from file_io import save_list_to_file
+from file_io import read_or_create_config
 from gift import get_data
 from discord import sent_discord
+
+tn_percents, global_level = read_or_create_config()
+if tn_percents is None:
+   tn_percents = 90
+if global_level is None:
+   global_level = 1
 
 def find_difference(original_data, updated_data):
     added_data = [line for line in updated_data if line not in original_data]
     removed_data = [line for line in original_data if line not in updated_data]
     return added_data, removed_data
+
+# 設定されたレベル(global_level)が与えられた(必要な)レベルを超えたときのみ通知
+def send_message(lv, text):
+    global global_level
+    if global_level >= lv:
+       sent_discord(text)
 
 get_list = []
 try:
@@ -26,7 +39,7 @@ for i in get_list:
       old_data_dict[str(i[0])] = l
     except FileNotFoundError as e:
       print(str(e))
-      sent_discord("新規 --> " + str(i[0]))
+      send_message(2, "新規 --> " + str(i[0]))
       new_id.append(i[0])
 
 for i in get_list:
@@ -38,19 +51,23 @@ for i in get_list:
     added = [' '.join(i) for i in added]
     removed = [' '.join(i) for i in removed]
 
-    added = '\n'.join(added)
-    removed = '\n'.join(removed)
+    # 差分通知
+    # Ctrl+K -> Ctrl+C (Ctrl+U)
+    added_str = '\n'.join(added)
+    removed_str = '\n'.join(removed)
     if added == "" and removed == "":
        continue
-
     s = f""" \
 {i[1]} 差分\n \
     ★削除データ\n \
-{removed}\n \
+{removed_str}\n \
     ★追加データ\n \
-{added}\n \
+{added_str}\n \
     """
-    sent_discord(s)
+    send_message(2, s)
+
+    # 追加データのうち、threshold_notificationを下回っているデータを通知
+    
 
 """
 今後
